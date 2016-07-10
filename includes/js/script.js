07581011535d21ba1cc17e8b42a5de4a19ef677d
InderjeetSingh14 <inderjeet.singh14@wipro.com>
2016-07-10 19:04:17 +0530

    secondExercise commit

---
+++ includes/js/script.js
@@ -0,0 +1,15 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
